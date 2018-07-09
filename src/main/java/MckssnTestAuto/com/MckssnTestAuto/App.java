5ec7e2c3057a5111e588fba23bc8f50dbc860951
Mayte <mtcm2711@gmail.com>
2018-07-09 10:54:09 -0500

    Changes1

--- src/main/java/MckssnTestAuto/com/MckssnTestAuto/App.java
+++ src/main/java/MckssnTestAuto/com/MckssnTestAuto/App.java
@@ -5,11 +5,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
- * Hello world!  branch1
+ * Hello world!  branch
  *
  */
 public class App 
@@ -23,18 +22,14 @@ public class App
        driver.get("https://devwcm-preview.mckesson.com/portal/site/smo");
        
        System.out.println("Browser opened");
-      // WebElement LoginForm;
-       
-     //  LoginForm = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("class[@login-form]")));
-       
        driver.findElement(By.id("username")).sendKeys("devngp06");
        driver.findElement(By.id("password")).sendKeys("Password1");
        driver.findElement(By.className("button")).click();
        System.out.println("Account accessed");
        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("a[@title='Ordering']")));
-       WebElement NGO = driver.findElement(By.id("mmlink0"));
-       action.moveToElement(NGO).click().perform();
-       driver.findElement(By.xpath("//*[@id='lnk9']/table/tbody/tr/td[1]/div/a")).click();
+       WebElement NGO = driver.findElement(By.xpath("//*[@id=\"wcm-left180\"]/div[4]/ul[1]/li[1]"));
+       action.moveToElement(NGO).click();
+       //driver.findElement(By.xpath("//*[@id='lnk9']/table/tbody/tr/td[1]/div/a")).click();
        System.out.println("NGO opened");
        
     }
